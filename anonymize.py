0d75203536d59c92ec2ff5dac98427d600e4a191
John <johnv@uchicago.edu>
2023-07-18 10:47:37 -0500

    removes error in anon.py

--- anonymize.py
+++ anonymize.py
@@ -20,9 +20,9 @@ for sub in sub_ids:
     sub_dir_new = os.path.join(ANON_DIR, 'sub-%s'%sub)
     shutil.copytree(sub_dir_old, sub_dir_new)
 
-    df = pd.read_csv(os.path.join(BIDS_DIR, 'participants.tsv'), sep = '\t', dtype = str)
-    data_complete = df.participant_id.isin(sub_ids)
-    # remove columns with identifying info (e.g. prolific worker ID)
-    df = df[data_complete][['participant_id', 'age', 'sex']]
-    tsv_f = os.path.join(ANON_DIR, 'participants.tsv')
-    df.to_csv(tsv_f, sep = '\t', na_rep = 'n/a', index = False)
+df = pd.read_csv(os.path.join(BIDS_DIR, 'participants.tsv'), sep = '\t', dtype = str)
+data_complete = df.participant_id.isin(sub_ids)
+# remove columns with identifying info (e.g. prolific worker ID)
+df = df[data_complete][['participant_id', 'age', 'sex']]
+tsv_f = os.path.join(ANON_DIR, 'participants.tsv')
+df.to_csv(tsv_f, sep = '\t', na_rep = 'n/a', index = False)
